927882677891e023b960bb23106bdb58fc4ebacd
Grovre <50428844+Grovre@users.noreply.github.com>
2023-11-18 13:15:55 -0600

    Byte and (u)int32 tests

---
+++ swed32-master/Testing/ByteTests.cs
@@ -0,0 +1,33 @@
+﻿namespace Testing;
+
+public unsafe class ByteTests
+{
+    private Swed _swed = new(Process.GetCurrentProcess());
+    private const int BytesLength = 451_723;
+    private byte[] _bytes = new byte[BytesLength];
+
+    [Test]
+    public void ReadBytesTest()
+    {
+        fixed (byte* b0 = &_bytes[0])
+        {
+            var safePtr = new IntPtr(b0);
+            var buffer = _swed.ReadBytes(safePtr, _bytes.Length);
+
+            Assert.That(buffer, Is.EquivalentTo(_bytes));
+        }
+    }
+
+    [Test]
+    public void WriteBytesTest()
+    {
+        fixed (byte* b0 = &_bytes[0])
+        {
+            var safePtr = new IntPtr(b0);
+            var written = _swed.WriteBytes(safePtr, new byte[_bytes.Length]);
+            
+            Assert.That(written, Is.True);
+            Assert.That(Array.TrueForAll(_bytes, b => b == 0), Is.True);
+        }
+    }
+}
---
+++ swed32-master/Testing/IntTests.cs
@@ -0,0 +1,60 @@
+﻿namespace Testing;
+
+[TestFixture]
+public unsafe class IntTests
+{
+    private byte[] _bytes = new byte[sizeof(int)];
+    private Swed _swed = new(Process.GetCurrentProcess());
+
+    [Test]
+    public void ReadIntTest()
+    {
+        var n = Random.Shared.Next();
+        var nBytes = BitConverter.GetBytes(n);
+        Assert.That(_bytes.Length, Is.EqualTo(nBytes.Length));
+        _bytes = nBytes;
+        
+        fixed (byte* b0 = &_bytes[0])
+        {
+            var safePtr = new IntPtr(b0);
+            var read = _swed.ReadInt(safePtr);
+            Assert.That(read, Is.EqualTo(n));
+        }
+    }
+
+    [Test]
+    public void WriteIntTest()
+    {
+        var n = Random.Shared.Next();
+        var safePtr = new IntPtr(&n);
+        var read = _swed.ReadInt(safePtr);
+
+        Assert.That(read, Is.EqualTo(n));
+    }
+    
+    [Test]
+    public void ReadUIntTest()
+    {
+        var n = (uint)Random.Shared.Next();
+        var nBytes = BitConverter.GetBytes(n);
+        Assert.That(_bytes.Length, Is.EqualTo(nBytes.Length));
+        _bytes = nBytes;
+        
+        fixed (byte* b0 = &_bytes[0])
+        {
+            var safePtr = new IntPtr(b0);
+            var read = _swed.ReadInt(safePtr);
+            Assert.That(read, Is.EqualTo(n));
+        }
+    }
+
+    [Test]
+    public void WriteUIntTest()
+    {
+        var n = (uint)Random.Shared.Next();
+        var safePtr = new IntPtr(&n);
+        var read = _swed.ReadInt(safePtr);
+
+        Assert.That(read, Is.EqualTo(n));
+    }
+}
