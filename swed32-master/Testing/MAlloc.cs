6d6119e604cf4c2b23af8b62b3a493cbbc0f8daf
grovre <landonvee111@gmail.com>
2023-12-02 19:45:19 -0600

    Added zeroing and randomization

--- swed32-master/Testing/MAlloc.cs
+++ swed32-master/Testing/MAlloc.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Numerics;
+using System.Runtime.InteropServices;
 
 namespace Testing;
 
@@ -9,10 +10,27 @@ public sealed unsafe class MAlloc<T> : IDisposable
     private readonly T* _buf;
     public Span<T> Span => new(_buf, Length);
 
-    public MAlloc(int length)
+    public MAlloc(int length, bool zero = true)
     {
         Length = length;
         _buf = (T*)Marshal.AllocHGlobal(sizeof(T) * length);
+        
+        if (zero)
+            Span.Clear();
+    }
+
+    public void RandomizeBytes(Random? rand = null)
+    {
+        rand ??= Random.Shared;
+        var bytes = MemoryMarshal.Cast<T, byte>(Span);
+        rand.NextBytes(bytes);
+    }
+
+    public void Randomize(Func<int, T> indexedRandomizer)
+    {
+        var span = Span;
+        for (var i = 0; i < span.Length; i++)
+            span[i] = indexedRandomizer(i);
     }
 
     public MAlloc<T> Copy()
