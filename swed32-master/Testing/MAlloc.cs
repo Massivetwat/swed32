930f632ebfc399d0da2aa98735bb31f3e2457f75
grovre <landonvee111@gmail.com>
2023-12-02 19:35:45 -0600

    Create MAlloc class for testing

---
+++ swed32-master/Testing/MAlloc.cs
@@ -0,0 +1,40 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Testing;
+
+public sealed unsafe class MAlloc<T> : IDisposable
+    where T : unmanaged
+{
+    public int Length { get; }
+    private readonly T* _buf;
+    public Span<T> Span => new(_buf, Length);
+
+    public MAlloc(int length)
+    {
+        Length = length;
+        _buf = (T*)Marshal.AllocHGlobal(sizeof(T) * length);
+    }
+
+    public MAlloc<T> Copy()
+    {
+        var malloc = new MAlloc<T>(Length);
+        Span.CopyTo(malloc.Span);
+        return malloc;
+    }
+
+    private void ReleaseUnmanagedResources()
+    {
+        Marshal.FreeHGlobal(new IntPtr(_buf));
+    }
+
+    public void Dispose()
+    {
+        ReleaseUnmanagedResources();
+        GC.SuppressFinalize(this);
+    }
+
+    ~MAlloc()
+    {
+        ReleaseUnmanagedResources();
+    }
+}
